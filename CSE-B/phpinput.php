995553c1a38adddc4537a8623afa20948cb0ff7f
Nandini9634 <nandiniagarwal9634@gmail.com>
2020-01-22 14:52:36 +0530

    modified changes

--- CSE-B/phpinput.php
+++ CSE-B/phpinput.php
@@ -3,22 +3,7 @@ include('config.php');//to include file
 //require('confi7g.php');//agr file incude nhi hui n file ki need h to it will stop d program include will not stop
 //require_once('config.php');
  ?>
-<?php
 
-if (isset($_POST['btn'])){
-  echo $_POST['id'].'<br>';
-  echo $_POST['username'].'<br>';
-  echo $_POST['age'].'<br>';
-  echo $_POST['contact'].'<br>';
-  echo $_POST['email'].'<br>';
-}
-else {
-  echo "welcome guest";
-}
-
-
-  // associative array[dictionary]
- ?>
 
 <html lang="en" dir="ltr">
   <head>
@@ -26,7 +11,7 @@ else {
     <title>Input Form</title>
   </head>
   <body>
-    <form  action="phpinput.php" method="post">
+    <form  action="details.php" method="post">
       ID: <input name="id", type="text"
       placeholder="ENTER ID"><br>
       Name: <input type="text" name="username" placeholder="ENTER name"><br>
