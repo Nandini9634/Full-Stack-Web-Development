ba9a25c497c94922975e85e8488a8c0bf2d46354
Nandini9634 <nandiniagarwal9634@gmail.com>
2020-01-22 14:35:00 +0530

    modified changes

---
+++ CSE-B/phpinput.php
@@ -0,0 +1,34 @@
+<?php
+
+if (isset($_POST['btn'])){
+  echo $_POST['id'].'<br>';
+  echo $_POST['username'].'<br>';
+  echo $_POST['age'].'<br>';
+  echo $_POST['contact'].'<br>';
+  echo $_POST['email'].'<br>';
+}
+else {
+  echo "welcome guest";
+}
+
+
+  // associative array[dictionary]
+ ?>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Input Form</title>
+  </head>
+  <body>
+    <form  action="phpinput.php" method="post">
+      ID: <input name="id", type="text"
+      placeholder="ENTER ID"><br>
+      Name: <input type="text" name="username" placeholder="ENTER name"><br>
+      Age: <input type="text" name="age" placeholder="ENTER age"><br>
+      Contact: <input type="text" name="contact" placeholder="ENTER contact"><br>
+      Email: <input type="text" name="email" placeholder="ENTER email"><br>
+      <br><input type="submit" name='btn' >
+    </form>
+  </body>
+</html>
