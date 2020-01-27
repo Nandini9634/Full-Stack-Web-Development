cb33776b8113b02f7d6de56aafc8d8c4b51e1e8b
Nandini9634 <nandiniagarwal9634@gmail.com>
2020-01-27 12:03:30 +0530

    modified changes

--- CSE-B/details.php
+++ CSE-B/details.php
@@ -1,11 +1,17 @@
 <?php
+include('config.php');
+?>
 
+<?php
 if (isset($_POST['btn'])){
-  echo $_POST['id'].'<br>';
-  echo $_POST['username'].'<br>';
-  echo $_POST['age'].'<br>';
-  echo $_POST['contact'].'<br>';
-  echo $_POST['email'].'<br>';
+ echo "dear, ".$_POST['username']."you are welcome".'<br>';
+  $username= $_POST['username'];
+  $age= $_POST['age'];
+  $contact= $_POST['contact'];
+  $email= $_POST['email'];
+
+  $sql="insert into users(username,age,contact,email) VALUES ('$username','$age','$contact','$email')";
+  mysqli_query($conn,$sql);
 }
 else {
   echo "welcome guest";
--- CSE-B/phpinput.php
+++ CSE-B/phpinput.php
@@ -1,8 +1,7 @@
+
 <?php
-include('config.php');//to include file
-//require('confi7g.php');//agr file incude nhi hui n file ki need h to it will stop d program include will not stop
-//require_once('config.php');
- ?>
+include('config.php');
+?>
 
 
 <html lang="en" dir="ltr">
@@ -12,13 +11,12 @@ include('config.php');//to include file
   </head>
   <body>
     <form  action="details.php" method="post">
-      ID: <input name="id", type="text"
-      placeholder="ENTER ID"><br>
       Name: <input type="text" name="username" placeholder="ENTER name"><br>
       Age: <input type="text" name="age" placeholder="ENTER age"><br>
       Contact: <input type="text" name="contact" placeholder="ENTER contact"><br>
       Email: <input type="text" name="email" placeholder="ENTER email"><br>
       <br><input type="submit" name='btn' >
     </form>
+
   </body>
 </html>
