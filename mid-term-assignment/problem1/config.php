0acf6ad70a7ab9a0794779707d6eb8144ab07707
Nandini9634 <nandiniagarwal9634@gmail.com>
2020-03-21 23:03:06 +0530

    new changes

--- mid-term-assignment/problem1/config.php
+++ mid-term-assignment/problem1/config.php
@@ -3,4 +3,11 @@ $hostName = 'localhost';
 $userName = 'root';
 $password = '';
 $databaseName = 'cse2020';
+
+$conn = mysqli_connect($hostName, $userName, $password, $databaseName);
+if ($conn){
+}
+else{
+    echo "Connection failed!!" + mysqli_connect_error();
+}
 ?>
