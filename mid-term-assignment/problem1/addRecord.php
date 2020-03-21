0f40041a6531e37667707d7cac1bf8fa99ccc4e4
Nandini9634 <nandiniagarwal9634@gmail.com>
2020-03-21 23:06:41 +0530

    new changes

--- mid-term-assignment/problem1/addRecord.php
+++ mid-term-assignment/problem1/addRecord.php
@@ -1,3 +1,14 @@
 <?php
 include('config.php');
+
+<?php
+
+  $enroll=$_POST["enroll"];
+  $name=$_POST["name"];
+  $age=$_POST["age"];
+  $course=$_POST["course"];
+  $branch=$_POST["branch"];
+  $contact=$_POST["contact"];
+  $email_id=$_POST["email_id"];
+  $insert ="INSERT INTO add_details (`enroll`,`name`,`age`,`course`,`branch`,`contact`,`email_id`) VALUES ('$enroll','$name','$age', '$course', '$branch','$contact','$email_id');";
 ?>
