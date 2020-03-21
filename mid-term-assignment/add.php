ca17de65bb582166b3f12e29c9d8babe538f9efe
Nandini9634 <nandiniagarwal9634@gmail.com>
2020-03-21 21:56:44 +0530

    new file

---
+++ mid-term-assignment/add.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+    <title>Input Form</title>
+    </head>
+
+    <body>
+	<div>
+		<div>
+			<h1>Enter Details</h1>
+		</div>
+
+		<form action="addRecord.php" method="post">
+
+			<div>
+				<label>Enrollment Number</label>
+				<input type="text" name="enroll" placeholder="GE-172012584" required="required">
+			</div>
+
+			<div>
+				<label>Name</label>
+				<input type="text" name="name" placeholder="Nandini Agarwal" required="required">
+			</div>
+
+			<div>
+				<label>Age</label>
+				<input type="number" name="age" placeholder="20" required="required">
+			</div>
+
+			<div>
+				<label>Course</label>
+				<input type="text" name="course" placeholder="B.Tech." required="required">
+			</div>
+
+			<div>
+				<label>Branch</label>
+				<input type="text" name="branch" placeholder="Computer Science" required="required">
+			</div>
+
+			<div>
+				<label>Contact</label>
+				<input type="number" name="contact" placeholder="9634889767" required="required">
+			</div>
+
+			<div>
+				<label>Email Id</label>
+				<input type="email_id" name="email_id" placeholder="nandiniagarwal9634@gmail.com" required="required">
+			</div>
+			<div>
+				<button type="submit" name="submit">Submit</button>
+			</div>
+		</form>
+	</div>
+</body>
+</html>
