897a7d95bc6c16d701c8f3b91697b45c45bc2bb3
Nandini9634 <nandiniagarwal9634@gmail.com>
2020-03-22 13:04:16 +0530

    new changes

--- mid-term-assignment/problem1/details.php
+++ mid-term-assignment/problem1/details.php
@@ -5,6 +5,21 @@
 <title>Records</title>
 </head>
 <body>
-  
+  <table border="1">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Enrollment Number</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Course</th>
+            <th>Branch</th>
+            <th>Contact</th>
+            <th>Email Id</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+    </thead>
+    </table>
 </body>
 </html>
