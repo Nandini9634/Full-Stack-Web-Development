34ab1cdbd617d81bd130e74887cd84a2c5da209d
Nandini9634 <nandiniagarwal9634@gmail.com>
2020-03-22 13:25:16 +0530

    new changes

--- mid-term-assignment/problem1/edit.php
+++ mid-term-assignment/problem1/edit.php
@@ -3,3 +3,15 @@ include('config.php');
 ?>
 <?php $id=$_GET['id'];
 ?>
+<?php
+    $sql = "SELECT * FROM `add_details` WHERE id='$id'";
+    $result = mysqli_query($conn, $sql);
+    $data = $result->fetch_assoc();
+    $enroll=$data["enroll"];
+    $name=$data["name"];
+    $age=$data["age"];
+    $course=$data["course"];
+    $branch=$data["branch"];
+    $contact=$data["contact"];
+    $email_id=$data["email_id"];
+?>
